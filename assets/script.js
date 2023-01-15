73204042be012aba2e25cdbf21ba951e5f669cee
andrew <ammassey42@gmail.com>
2023-01-15 14:50:35 -0800

    started info

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+let gimpNames = ["MyroTheGimp", "Homage49", "Chickenbobv0", "GIMofAnarchy", "gimpy4u"]
+
+//loop through array and get stats
+
+for (let i = 0; i < gimpNames.length; i++) {
+    const element = gimpNames[i];
+    
+}
