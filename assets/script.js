65ec9cd74f24ed80d9393cd2e4908914890ace2a
andrew <ammassey42@gmail.com>
2023-01-20 21:06:10 -0800

    updates

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+const { constants, hiscores } = require("osrs-api");
 let gimpNames = ["MyroTheGimp", "Homage49", "Chickenbobv0", "GIMofAnarchy", "gimpy4u"]
 
 //loop through array and get stats
@@ -6,3 +7,5 @@ for (let i = 0; i < gimpNames.length; i++) {
     const element = gimpNames[i];
     
 }
+
+//
